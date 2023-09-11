6380c4150d0aa16558a8d27714e3a446335035a7
Dooik <power1231002@naver.com>
2023-09-11 12:37:22 +0900

    230911

---
+++ baekjoon/silver4/2670/2670.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    double arr[10000];
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    double max = 0;
+    for(int i = 0; i < n; i++) {
+        
+    }
+
+    
+}
